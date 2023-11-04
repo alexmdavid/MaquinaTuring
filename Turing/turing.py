bfe4958663c78cac393c3d89836db3015dba060b
DEIVIS JESID OLASCOAGA SALCEDO <deivisolascoagajs@unimagdalena.edu.co>
2023-11-04 17:41:59 -0500

    avances

--- Turing/turing.py
+++ Turing/turing.py
@@ -6,7 +6,6 @@ class TuringMachineSimulator:
         self.root.title("Máquina de Turing Simulada")
 
         self.cinta = [" "," "," "," ","0", "1", "0", "1", "0", "1", "0","1"," "," "," "," "]
-        self.head_position = 0
 
         
 
@@ -16,21 +15,27 @@ class TuringMachineSimulator:
         self.cells = [tk.Label(self.frame_cinta, text=char, font=("Courier", 20), width=2, borderwidth=1, relief="solid") for char in self.cinta]
         for cell in self.cells:
             cell.pack(side="left")
-
+        
         self.label_head = tk.Label(root, text="▲", font=("Courier", 15))
-        self.indice_cinta=self.position_head_sycronized()
-        self.label_head.place(x=self.indice_cinta, y=38)  # Posición inicial de la cabeza
+        self.head_position = self.search_head()
+        
+        self.label_head.place(x=self.head_position, y=38)  # Posición inicial de la cabeza
 
         self.step_delay = 1000  # Intervalo de tiempo en milisegundos
         self.auto_step()
         
 
-    def position_head_sycronized(self):
+    def search_head(self):
         for i in range(len(self.cinta)):
             if self.cinta [i] != " ":
                 return i
                 
                 
+    def position_head_sycronized(self, x):
+        self.cell_coordinates[x]
+        print(self.cell_coordinates[x])
+
+
     
 
     def auto_step(self):
@@ -49,12 +54,14 @@ class TuringMachineSimulator:
             self.cells[i].config(text=self.cinta[i])
 
         # Mueve la cabeza hacia abajo
-        self.label_head.place(x=self.indice_cinta +36 * self.head_position, y=38)
+        self.label_head.place(x=self.head_position +36 * self.head_position, y=38)
 
         # Programa el siguiente paso automático
         self.root.after(self.step_delay, self.auto_step)
-        cell_coordinates = [cell.winfo_x() for cell in self.cells]
-        print("Coordenadas X de las celdas:", cell_coordinates)
+
+        self.cell_coordinates = [cell.winfo_x() for cell in self.cells]
+        self.position_head_sycronized(self.head_position)
+        
 
 
 if __name__ == "__main__":
