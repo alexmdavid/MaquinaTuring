afa501deb376ffb3cc83132059237a54f8786d66
alexdavid <joelarga2020@gmail.com>
2023-11-04 23:28:37 -0500

    alineando la cabeza de lectura/escrituta al simbolo correspondiente

--- Turing/turing.py
+++ Turing/turing.py
@@ -4,72 +4,58 @@ class TuringMachineSimulator:
     def __init__(self, root):
         self.root = root
         self.root.title("Máquina de Turing Simulada")
-
-        self.cinta = [" "," "," "," ","0", "1", "0", "1", "0", "1", "0","1"," "," "," "," "]
-
-        
-
+        self.cinta = ["1", "1", "0", "1", "0", "1", "0", "1"]
         self.frame_cinta = tk.Frame(root)
         self.frame_cinta.pack()
-
         self.cells = [tk.Label(self.frame_cinta, text=char, font=("Courier", 20), width=2, borderwidth=1, relief="solid") for char in self.cinta]
+
+        self.label_head = tk.Label(root, text="▲", font=("Courier", 15))
+        self.head_position = 0
+        self.step_delay = 1000  # Intervalo de tiempo en milisegundos
+
         for cell in self.cells:
             cell.pack(side="left")
-        
-        self.label_head = tk.Label(root, text="▲", font=("Courier", 15))
-        self.head_position = self.search_head()
-        
-        self.label_head.place(x=self.head_position, y=38)  # Posición inicial de la cabeza
 
-        self.step_delay = 1000  # Intervalo de tiempo en milisegundos
         self.auto_step()
-        
 
-    def search_head(self):
-        for i in range(len(self.cinta)):
-            if self.cinta [i] != " ":
-                return i
-                
-                
-    def position_head_sycronized(self, x):
-        self.cell_coordinates[x]
-        print(self.cell_coordinates[x])
-
-
-    
-
-    def auto_step(self):
-        current_symbol = self.cinta[self.head_position]
-        if current_symbol == '0':
-            self.cinta[self.head_position] = '1'
-            self.head_position += 1
-        else:
-            if current_symbol == '1':
+    def actualizar_cinta(self):
+        if self.head_position < len(self.cinta):
+            current_symbol = self.cinta[self.head_position]
+            if current_symbol == '0':
+                self.cinta[self.head_position] = '1'
+                self.head_position += 1
+            elif current_symbol == '1':
                 self.cinta[self.head_position] = '0'
                 self.head_position += 1
 
+            # Mueve la cabeza debajo de la celda actual
+            self.mover_cabeza(self.head_position)
 
         # Actualiza el texto en las celdas
-        for i in range(len(self.cinta)):
-            self.cells[i].config(text=self.cinta[i])
+        for j in range(0, len(self.cells)):
+            self.cells[j].config(text=self.cinta[j])
 
-        # Mueve la cabeza hacia abajo
-        self.label_head.place(x=self.head_position +36 * self.head_position, y=38)
+        if self.head_position >= len(self.cinta):
+            self.detener_simulacion()  # Detener la simulación si se llegó al final de la cinta
 
-        # Programa el siguiente paso automático
-        self.root.after(self.step_delay, self.auto_step)
-
-        self.cell_coordinates = [cell.winfo_x() for cell in self.cells]
-        self.position_head_sycronized(self.head_position)
+    def detener_simulacion(self):
         
+        pass
 
+    def mover_cabeza(self, index):
+        for cell in self.cells:
+            cell.config(borderwidth=1, relief="solid")  # Restaura el estilo de todas las celdas
+        if index < len(self.cinta):
+            self.cells[index].config(borderwidth=2, relief="raised")  # Destaca la celda actual, ey perro hace la celda mas gruesa
+            self.label_head.place(in_=self.cells[index], bordermode="outside", relx=0.5, rely=1.0, y=3)
+
+    def auto_step(self):
+        self.actualizar_cinta()
+        self.root.after(self.step_delay, self.auto_step)
 
 if __name__ == "__main__":
     root = tk.Tk()
-    # Establecemos el tamaño mínimo
     root.minsize(1200, 600)
-
-    # Establecemos el tamaño máximo
     root.maxsize(1200, 600)
     app = TuringMachineSimulator(root)
     root.mainloop()
